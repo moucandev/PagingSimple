1844ba0c0fc9ff5c6e5a0dc0376249db1be10215
moucan <811048803@qq.com>
2021-03-25 18:09:09 +0800

    补充提交

---
+++ app/build/tmp/kapt3/stubs/debug/com/moucan/pagingsimple/MainViewModel.java
@@ -0,0 +1,16 @@
+package com.moucan.pagingsimple;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 4, 2}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\u0003\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\u00060\u00050\u0004\u00a8\u0006\u0007"}, d2 = {"Lcom/moucan/pagingsimple/MainViewModel;", "Lcom/moucan/common/base/BaseViewModel;", "()V", "getData", "Lkotlinx/coroutines/flow/Flow;", "Landroidx/paging/PagingData;", "Lcom/moucan/common/http/GitRepo;", "app_debug"})
+public final class MainViewModel extends com.moucan.common.base.BaseViewModel {
+    
+    @org.jetbrains.annotations.NotNull()
+    public final kotlinx.coroutines.flow.Flow<androidx.paging.PagingData<com.moucan.common.http.GitRepo>> getData() {
+        return null;
+    }
+    
+    public MainViewModel() {
+        super();
+    }
+}
